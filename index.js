c7a2c7cde0aef67f3aab2f641a6c3498adfe4423
Guidry <jh_guidry@outlook.com>
2019-10-28 14:32:00 -0500

    invalid error styling for placeholder complete and hover state for lightened button complete

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const form = document.querySelector(".signup-form");
-// const fNameInput = document.querySelector("#fname");
-// const lNameInput = document.querySelector("#lname");
-// const emailInput = document.querySelector("#email");
-// const password = document.querySelector("#password");
 const inputForms = document.querySelectorAll(".errors");
 const submitButton = document.querySelector("#submit");
-
-
 
 // Suppress the default error popup bubbles
 form.addEventListener( "invalid", function( event ) {
@@ -24,21 +18,13 @@ const invalid = (e) => {
         //clear field before testing validity
         event.target.setCustomValidity("");
 
-        // if input is invalid, toggle the paragraph error
+        // if input is invalid, toggle the error styles
         if(!event.target.validity.valid) {
             event.target.classList.toggle("error-input");
-            //event.target.classList.toggle(".input-placeholder::placeholder");
             event.target.nextElementSibling.classList.toggle("error-icon-display");
             event.target.nextElementSibling.nextElementSibling.classList.toggle("error-display");
         } 
-        // else 
-        if(event.target.nextElementSibling.nextElementSibling.classList.contains("error-display")
-        && event.target.validity.valid) {
-            event.target.classList.toggle("error-input");
-            //event.target.classList.toggle(".input-placeholder::placeholder");
-            event.target.nextElementSibling.classList.toggle("error-icon-display");
-            event.target.nextElementSibling.nextElementSibling.classList.toggle("error-display");
-        }
+
     };
     e.target.oninput = (event) => {
         //clear field after
